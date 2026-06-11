17287c0c093fa6d060c989bc3eeb4786ef89baa9
hammad9876421 <freperede@gmail.comhttps://github.com/hammad9876421/modpack-builder>
2026-06-12 02:36:53 +0500

    merge fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
 });
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+>>>>>>> af0d928 (initial setup)
